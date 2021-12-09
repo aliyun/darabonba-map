8481b6001c94064172b19016cc7c6a2ef694d28f
yndu13 <yndu13@gmail.com>
2021-12-09 10:14:53 +0800

    case IDictionary

--- csharp/core/MapUtil.cs
+++ csharp/core/MapUtil.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 using Tea;
@@ -18,15 +19,24 @@ namespace AlibabaCloud.DarabonbaMap
     public class MapUtil 
     {
 
-        public static int? Size(Dictionary<string, object> raw)
+        public static int Size(IDictionary raw)
         {
+            if (raw == null)
+            {
+                return 0;
+            }
             return raw.Count;
         }
 
-        public static List<string> KeySet(Dictionary<string, object> raw)
+        public static List<string> KeySet(IDictionary raw)
         {
             List<string> keyList = new List<string>();
-            foreach (KeyValuePair<string, object> kvp in raw)
+            if (raw == null)
+            {
+                return keyList;
+            }
+            Dictionary<string, object> dict = raw.Keys.Cast<string>().ToDictionary(key => key, key => raw[key]);
+            foreach (KeyValuePair<string, object> kvp in dict)
             {
                 keyList.Add(kvp.Key);
             }
